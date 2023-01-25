3ec38f2facdb51476ad9f170c2a264c342afbd79
Bilal Hassan <bilalhassain14@gmail.com>
2023-01-25 14:55:05 +0500

    fixed responsiveness and added loading message

--- src/components/AllStudents.js
+++ src/components/AllStudents.js
@@ -7,6 +7,7 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 function AllStudents() {
   document.title = "STUDENTReg - Registered Students";
   const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleDelete = (student) => {
     axios.delete(`https://studentsreg-backend.cyclic.app/${student._id}`);
@@ -32,9 +33,11 @@ function AllStudents() {
   }, []);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
       const record = await axios.get("https://studentsreg-backend.cyclic.app/");
       setStudents(record.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -52,72 +55,86 @@ function AllStudents() {
         />
       </div>
       <hr />
-
-      {students.length !== 0 ? (
-        <table className="table table-sm table-striped">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone #</th>
-              <th>DOB</th>
-              <th>City</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student) => {
-              return (
-                <tr key={student._id}>
-                  <th scope="row">{student._id.slice(5, 7)}</th>
-                  <td>{student.name}</td>
-                  <td>{student.email}</td>
-                  <td>{student.phone}</td>
-                  <td>{student.dob}</td>
-                  <td>{student.city}</td>
-                  <td>
-                    <Link to={`/editstudent/${student._id}`}>
-                      {" "}
-                      <i title="Edit" className="icon edit-icon">
-                        <FaEdit />
-                      </i>
-                    </Link>
-                    &nbsp;&nbsp;
-                    <i
-                      title="Delete"
-                      onClick={() =>
-                        window.confirm(
-                          `Are you sure to delete ${student.name}'s data`
-                        )
-                          ? handleDelete(student)
-                          : null
-                      }
-                      className="icon delete-icon"
-                    >
-                      <RiDeleteBin6Line />
-                    </i>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      ) : (
+      {loading ? (
         <h5
           style={{ color: "rgb(150, 150, 150)" }}
           className="my-3 mt-5 display-6 container-fluid"
         >
-          No Record
+          Loading...
         </h5>
+      ) : (
+        <div>
+          {students.length !== 0 ? (
+            <div className="my-table">
+              <table className="table table-sm table-striped">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone #</th>
+                    <th>DOB</th>
+                    <th>City</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {students.map((student) => {
+                    return (
+                      <tr key={student._id}>
+                        <th scope="row">{student._id.slice(5, 7)}</th>
+                        <td>{student.name}</td>
+                        <td>{student.email}</td>
+                        <td>{student.phone}</td>
+                        <td>{student.dob}</td>
+                        <td>{student.city}</td>
+                        <td>
+                          <Link to={`/editstudent/${student._id}`}>
+                            {" "}
+                            <i title="Edit" className="icon edit-icon">
+                              <FaEdit />
+                            </i>
+                          </Link>
+                          &nbsp;&nbsp;
+                          <i
+                            title="Delete"
+                            onClick={() =>
+                              window.confirm(
+                                `Are you sure to delete ${student.name}'s data`
+                              )
+                                ? handleDelete(student)
+                                : null
+                            }
+                            className="icon delete-icon"
+                          >
+                            <RiDeleteBin6Line />
+                          </i>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <h5
+              style={{ color: "rgb(150, 150, 150)" }}
+              className="my-3 mt-5 display-6 container-fluid"
+            >
+              No Record
+            </h5>
+          )}
+          <div>
+            <Link to="/addstudent">
+              <input
+                type="button"
+                className="form-control btn my-btn"
+                value="Add Student"
+              />
+            </Link>
+          </div>
+        </div>
       )}
-      <Link to="/addstudent">
-        <input
-          type="button"
-          className="form-control btn my-btn"
-          value="Add Student"
-        />
-      </Link>
     </div>
   );
 }
--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -6,6 +6,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 function EditStudent() {
   document.title = "STUDENTReg - Edit Student";
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [showPass, setShowPass] = useState(false);
   const id = window.location.pathname.split("/").pop();
@@ -61,11 +62,13 @@ function EditStudent() {
 
   const fetchData = async () => {
     const id = window.location.pathname.split("/").pop();
+    setLoading(true);
     try {
       const record = await axios.get(
         `https://studentsreg-backend.cyclic.app/${id}`
       );
       setInitialValues(record.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -79,123 +82,135 @@ function EditStudent() {
     <div className="container p-3 my-3">
       <h3>Edit details</h3>
       <hr />
-      <form onSubmit={handleSubmit}>
-        <div className="form-group mt-2">
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Enter your name"
-          />
-        </div>
-        {errors.name && touched.name ? (
-          <span className="input-errors">{errors.name}</span>
-        ) : null}
-        <div className="form-group mt-2">
-          <input
-            type="text"
-            name="email"
-            className="form-control"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Enter your email"
-          />
-        </div>
-        {errors.email && touched.email ? (
-          <span className="input-errors">{errors.email}</span>
-        ) : null}
-        <span className="input-errors"> {error !== false ? error : null}</span>
-        <div className="form-group mt-2 ">
-          <div className="d-flex justify-content-row ">
+      {loading ? (
+        <h5
+          style={{ color: "rgb(150, 150, 150)" }}
+          className="my-3 mt-5 display-6 container-fluid"
+        >
+          Please wait...
+        </h5>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group mt-2">
             <input
-              type={showPass === false ? "password" : "text"}
-              name="password"
+              type="text"
+              name="name"
               className="form-control"
-              value={values.password}
+              value={values.name}
               onChange={handleChange}
               onBlur={handleBlur}
-              placeholder="Enter your password"
-            />
-            <input
-              type="button"
-              className="btn btn-sm btn-light ms-2"
-              value={showPass === false ? "Show Password" : "Hide Password"}
-              onClick={handlePassStatus}
+              placeholder="Enter your name"
             />
           </div>
-        </div>
-        {errors.password && touched.password ? (
-          <span className="input-errors">{errors.password}</span>
-        ) : null}
-        <div className="form-group mt-2">
-          <input
-            type={showPass === false ? "password" : "text"}
-            name="cpassword"
-            className="form-control"
-            value={values.cpassword}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Confirm password"
-          />
-        </div>
-        {errors.cpassword && touched.cpassword ? (
-          <span className="input-errors">{errors.cpassword}</span>
-        ) : null}
-        <div className="form-group mt-2">
-          <input
-            type="number"
-            name="phone"
-            className="form-control"
-            value={values.phone}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Enter your number"
-          />
-        </div>
-        {errors.phone && touched.phone ? (
-          <span className="input-errors">{errors.phone}</span>
-        ) : null}
-        <div className="form-group mt-2">
-          <input
-            type="date"
-            name="dob"
-            className="form-control"
-            value={values.dob}
-            onChange={handleChange}
-          />
-        </div>
-        {errors.dob && touched.dob ? (
-          <span className="input-errors">{errors.dob}</span>
-        ) : null}
-        <div className="form-group mt-2">
-          <input
-            type="text"
-            name="city"
-            className="form-control"
-            value={values.city}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Enter your city"
-          />
-        </div>
-        {errors.city && touched.city ? (
-          <span className="input-errors">{errors.city}</span>
-        ) : null}
-        <div className="mt-3">
-          <input className="btn btn-success" type="submit" value="Update" />
-          <Link to="/">
+          {errors.name && touched.name ? (
+            <span className="input-errors">{errors.name}</span>
+          ) : null}
+          <div className="form-group mt-2">
             <input
-              type="button"
-              value="Cancel"
-              className="btn btn-light ms-2"
+              type="text"
+              name="email"
+              className="form-control"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Enter your email"
             />
-          </Link>
-        </div>
-      </form>
+          </div>
+          {errors.email && touched.email ? (
+            <span className="input-errors">{errors.email}</span>
+          ) : null}
+          <span className="input-errors">
+            {" "}
+            {error !== false ? error : null}
+          </span>
+          <div className="form-group mt-2 ">
+            <div className="d-flex justify-content-row ">
+              <input
+                type={showPass === false ? "password" : "text"}
+                name="password"
+                className="form-control"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Enter your password"
+              />
+              <input
+                type="button"
+                className="btn btn-sm btn-light ms-2"
+                value={showPass === false ? "Show Password" : "Hide Password"}
+                onClick={handlePassStatus}
+              />
+            </div>
+          </div>
+          {errors.password && touched.password ? (
+            <span className="input-errors">{errors.password}</span>
+          ) : null}
+          <div className="form-group mt-2">
+            <input
+              type={showPass === false ? "password" : "text"}
+              name="cpassword"
+              className="form-control"
+              value={values.cpassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Confirm password"
+            />
+          </div>
+          {errors.cpassword && touched.cpassword ? (
+            <span className="input-errors">{errors.cpassword}</span>
+          ) : null}
+          <div className="form-group mt-2">
+            <input
+              type="number"
+              name="phone"
+              className="form-control"
+              value={values.phone}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Enter your number"
+            />
+          </div>
+          {errors.phone && touched.phone ? (
+            <span className="input-errors">{errors.phone}</span>
+          ) : null}
+          <div className="form-group mt-2">
+            <input
+              type="date"
+              name="dob"
+              className="form-control"
+              value={values.dob}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.dob && touched.dob ? (
+            <span className="input-errors">{errors.dob}</span>
+          ) : null}
+          <div className="form-group mt-2">
+            <input
+              type="text"
+              name="city"
+              className="form-control"
+              value={values.city}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Enter your city"
+            />
+          </div>
+          {errors.city && touched.city ? (
+            <span className="input-errors">{errors.city}</span>
+          ) : null}
+          <div className="mt-3">
+            <input className="btn btn-success" type="submit" value="Update" />
+            <Link to="/">
+              <input
+                type="button"
+                value="Cancel"
+                className="btn btn-light ms-2"
+              />
+            </Link>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
