3eb9096e0c5bce854a42d82f05995b7ac29c63d1
Bilal Hassan <bilalhassain14@gmail.com>
2023-02-15 15:35:51 +0500

    minor fixes in edit form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,3 @@
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
